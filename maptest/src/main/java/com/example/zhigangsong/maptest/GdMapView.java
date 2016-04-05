71b7cc5ae8fad3ef077e1bfe47e3a4cc892463d0
zhigang.song <zhigang.song@moji.com>
2016-04-05 15:07:46 +0800

    customView

---
+++ maptest/src/main/java/com/example/zhigangsong/maptest/GdMapView.java
@@ -0,0 +1,267 @@
+package com.example.zhigangsong.maptest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdate;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.CameraPosition;
+import com.amap.api.maps2d.model.GroundOverlay;
+import com.amap.api.maps2d.model.GroundOverlayOptions;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
+
+import java.util.Calendar;
+
+/**
+ * Created by zhigang.song on 2016/4/5.
+ */
+public class GdMapView extends FrameLayout implements View.OnClickListener, AMap.OnMapClickListener, AMap.OnCameraChangeListener {
+    private static final String TAG = "huli";
+    private MapView mMapView;
+    private Button mBtnPlayControl;
+    private AMap mMap;
+    private TextView mTimeLine;
+
+    AMapLocationListener mLocationListener;
+    private Context ctx;
+    private CameraPosition mCameraPosition;
+    private float mZoomLevel = 5;
+    private Marker mLastMarker;
+    private AMapLocationClient mLocationClient;
+    private GeocodeSearch mGeocodeSearch;
+    private RadarPlayer mPlayer;
+    private GroundOverlay mLastGroundOverlay;
+
+
+    public GdMapView(Context context) {
+        this(context, null);
+    }
+
+    public GdMapView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GdMapView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        ctx = context;
+    }
+
+    private void initData() {
+        PlayHandler playHandler = new PlayHandler(this);
+        mLocationClient = new AMapLocationClient(ctx);
+        mPlayer = new RadarPlayer(playHandler);
+        mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                LatLng latLng = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+                mCameraPosition = CameraPosition.fromLatLngZoom(latLng, mZoomLevel);
+                CameraUpdate update = CameraUpdateFactory.newCameraPosition(mCameraPosition);
+                mMap.moveCamera(update);
+                Log.e(TAG, "my location is: " + latLng.toString());
+                updateLocation(latLng);
+            }
+        };
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        option.setOnceLocation(true).setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationClient.setLocationOption(option);
+        mLocationClient.setLocationListener(mLocationListener);
+        mGeocodeSearch = new GeocodeSearch(ctx);
+        mGeocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+            @Override
+            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+                Log.d(TAG, regeocodeResult.getRegeocodeAddress().getFormatAddress());
+                LatLonPoint point = regeocodeResult.getRegeocodeQuery().getPoint();
+                LatLng latLng = new LatLng(point.getLatitude(), point.getLongitude());
+                String pos = regeocodeResult.getRegeocodeAddress().getFormatAddress();
+                updateMarker(latLng, pos);
+            }
+
+            @Override
+            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+            }
+        });
+    }
+
+    private void updateMarker(LatLng latLng, String title) {
+        removeMarkers();
+        mLastMarker = mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .title(title));
+        mLastMarker.showInfoWindow();
+        notifyPlayerPositionChanged(latLng, mZoomLevel);
+    }
+
+    private void updateLocation(LatLng latLng) {
+        LatLonPoint point = new LatLonPoint(latLng.latitude, latLng.longitude);
+        RegeocodeQuery query = new RegeocodeQuery(point, 0, GeocodeSearch.AMAP);
+        mGeocodeSearch.getFromLocationAsyn(query);
+    }
+
+    private void notifyPlayerPositionChanged(LatLng latLng, float zoomLevel) {
+        mPlayer.changPosition(latLng, zoomLevel);
+    }
+
+    private void removeMarkers() {
+        if (null != mLastMarker) {
+            mLastMarker.remove();
+        }
+    }
+
+    private void initView(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.map_layout, this);
+        mMapView = (MapView) findViewById(R.id.map_view);
+        mMap = mMapView.getMap();
+        mBtnPlayControl = (Button) findViewById(R.id.play_control);
+        mBtnPlayControl.setOnClickListener(this);
+        mTimeLine = (TextView) findViewById(R.id.time_line);
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setMyLocationButtonEnabled(false);
+        uiSettings.setZoomControlsEnabled(false);
+        mMap.setOnMapClickListener(this);
+        mMap.setOnCameraChangeListener(this);
+    }
+
+    private void requestMyLocation() {
+        Log.i(TAG, "start request location");
+        mLocationClient.startLocation();
+    }
+
+
+    public void onCreate(Bundle bundle) {
+        initView(ctx);
+        initData();
+        Log.i(TAG, "map oncreate");
+        mMapView.onCreate(bundle);
+    }
+
+    public void onSaveInstanceState(Bundle bundle) {
+        Log.i(TAG, "map onsave");
+        mMapView.onSaveInstanceState(bundle);
+    }
+
+    public void onPause() {
+        Log.i(TAG, "map onpause");
+        mMapView.onPause();
+        mPlayer.pause();
+    }
+
+    public void onResume() {
+        mMapView.onResume();
+        Log.i(TAG, "map onResume");
+        requestMyLocation();
+        mPlayer.resume();
+    }
+
+    public void onDestory() {
+        Log.i(TAG, "map onDestory");
+        mMapView.onDestroy();
+        mPlayer.destory();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.play_control:
+                if (mPlayer.isPlay()){
+                    pausePlay();
+                }else {
+                    resumePlay();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void resumePlay() {
+        mBtnPlayControl.setText("pause");
+        mPlayer.resume();
+    }
+
+    private void pausePlay() {
+        mBtnPlayControl.setText("play");
+        mPlayer.pause();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        updateLocation(latLng);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+        mZoomLevel = cameraPosition.zoom;
+        notifyPlayerPositionChanged(cameraPosition.target, mZoomLevel);
+    }
+
+    static class PlayHandler extends NoLeakHandler<GdMapView> {
+        public PlayHandler(GdMapView outClass) {
+            super(outClass);
+        }
+
+        @Override
+        public void handleMessage(Message msg, GdMapView gdMapView) {
+            if (null != gdMapView) {
+                switch (msg.what) {
+                    case RadarPlayer.MSG_PLAY:
+                        RadarImageEntity image = (RadarImageEntity) msg.obj;
+                        gdMapView.playWav(image);
+                        break;
+                    case RadarPlayer.MSG_STOP:
+                        gdMapView.clearRadioImg();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void clearRadioImg() {
+
+    }
+
+
+    private void playWav(RadarImageEntity image) {
+        if (mLastGroundOverlay != null) {
+            mLastGroundOverlay.remove();
+        }
+        mLastGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .anchor(0.5f, 0.5f).transparency(0.1f)
+                .image(BitmapDescriptorFactory.fromPath(image.getCachePath()))
+                .positionFromBounds(image.getArea()));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis((long) (image.getTime() * 1000));
+        mTimeLine.setText(calendar.get(Calendar.HOUR_OF_DAY) + " : " + calendar.get(Calendar.MINUTE));
+    }
+}
--- maptest/src/main/java/com/example/zhigangsong/maptest/MainActivity.java
+++ maptest/src/main/java/com/example/zhigangsong/maptest/MainActivity.java
@@ -1,42 +1,21 @@
 package com.example.zhigangsong.maptest;
 
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.CameraUpdate;
-import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.UiSettings;
-import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.CameraPosition;
 import com.amap.api.maps2d.model.GroundOverlay;
-import com.amap.api.maps2d.model.GroundOverlayOptions;
-import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
-import com.amap.api.maps2d.model.MarkerOptions;
-import com.amap.api.services.core.LatLonPoint;
-import com.amap.api.services.core.PoiItem;
-import com.amap.api.services.geocoder.GeocodeResult;
 import com.amap.api.services.geocoder.GeocodeSearch;
-import com.amap.api.services.geocoder.RegeocodeQuery;
-import com.amap.api.services.geocoder.RegeocodeResult;
 
-import java.util.Calendar;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener, AMap.OnCameraChangeListener, GeocodeSearch.OnGeocodeSearchListener {
+public class MainActivity extends AppCompatActivity  {
 
     private Marker mLastMarker;
     CameraPosition mCameraPosition;
@@ -44,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
     public AMapLocationClient mLocationClient = null;
     GeocodeSearch mGeocodeSearch;
     GroundOverlay mLastGroundOverlay;
-    PlayHandler mPlayHandler = new PlayHandler(this);
+//    PlayHandler mPlayHandler = new PlayHandler(this);
     RadarPlayer mPlayer;
     Button mControlBtn;
     private static final String TAG = "huli";
@@ -55,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
     TextView mTimeLine;
     TextView mLoc;
     ImageView mImageView;
-
+    GdMapView mGdMapView;
 
 
 
@@ -63,210 +42,220 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        mMapView.onCreate(savedInstanceState);
-        mPlayer = new RadarPlayer(mPlayHandler);
-    }
-
-    //声明定位回调监听器
-    private AMapLocationListener mLocationListener = new AMapLocationListener() {
-        @Override
-        public void onLocationChanged(AMapLocation aMapLocation) {
-            LatLng latLng = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
-            mCameraPosition = CameraPosition.fromLatLngZoom(latLng, mZoomLevel);
-            CameraUpdate update = CameraUpdateFactory.newCameraPosition(mCameraPosition);
-            mAMap.moveCamera(update);
-            Log.i(TAG,"myloc is " + latLng.toString());
-            updateMarker(latLng);
+        mGdMapView = (GdMapView) findViewById(R.id.map);
+        if (mGdMapView != null) {
+            mGdMapView.onCreate(savedInstanceState);
         }
-    };
-
-
-    private void notifyPlayerPositionChanged() {
-        CameraPosition position = mAMap.getCameraPosition();
-        mPlayer.changPosition(position.target, position.zoom);
+//        initView();
+//        mMapView.onCreate(savedInstanceState);
+//        mPlayer = new RadarPlayer(mPlayHandler);
     }
+
+//    //声明定位回调监听器
+//    private AMapLocationListener mLocationListener = new AMapLocationListener() {
+//        @Override
+//        public void onLocationChanged(AMapLocation aMapLocation) {
+//            LatLng latLng = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+//            mCameraPosition = CameraPosition.fromLatLngZoom(latLng, mZoomLevel);
+//            CameraUpdate update = CameraUpdateFactory.newCameraPosition(mCameraPosition);
+//            mAMap.moveCamera(update);
+//            Log.i(TAG,"myloc is " + latLng.toString());
+//            updateMarker(latLng);
+//        }
+//    };
+
+
+//    private void notifyPlayerPositionChanged() {
+//        CameraPosition position = mAMap.getCameraPosition();
+//        mPlayer.changPosition(position.target, position.zoom);
+//    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mMapView.onResume();
-        requestMyLocation();
+//        mMapView.onResume();
+//        requestMyLocation();
+        mGdMapView.onResume();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mMapView.onDestroy();
-        mPlayer.destory();
+        mGdMapView.onDestory();
+//        mMapView.onDestroy();
+//        mPlayer.destory();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mMapView.onPause();
-        mLocationClient.onDestroy();
-        if (mPlayer != null) {
-            mPlayer.pause();
-        }
+        mGdMapView.onPause();
+//        mMapView.onPause();
+//        mLocationClient.onDestroy();
+//        if (mPlayer != null) {
+//            mPlayer.pause();
+//        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
+//        mMapView.onSaveInstanceState(outState);
+        mGdMapView.onSaveInstanceState(outState);
     }
-
-    private void updateMarker(LatLng latLng) {
-        removeMarkers();
-        LatLonPoint point = new LatLonPoint(latLng.latitude, latLng.longitude);
-        RegeocodeQuery query = new RegeocodeQuery(point, 0, GeocodeSearch.AMAP);
-        mGeocodeSearch.getFromLocationAsyn(query);
-        mLastMarker = mAMap.addMarker(new MarkerOptions().position(latLng).icon(
-                BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-        notifyPlayerPositionChanged();
-    }
-
-
-    private void initView() {
-        mMapView = (MapView) findViewById(R.id.map_view);
-        mLoc = (TextView) findViewById(R.id.loc_info);
-        mControlBtn = (Button) findViewById(R.id.play);
-        mGeocodeSearch = new GeocodeSearch(this);
-        mGeocodeSearch.setOnGeocodeSearchListener(this);
-        mImageView = (ImageView) findViewById(R.id.img);
-        mTimeLine = (TextView) findViewById(R.id.time_line);
-        initMap();
-    }
-
-    private void initMap() {
-        mAMap = mMapView.getMap();
-        mUiSettings = mAMap.getUiSettings();
-        mUiSettings.setZoomControlsEnabled(false);
-        mAMap.setOnMapClickListener(this);
-        mAMap.setOnCameraChangeListener(this);
-    }
-
-    private void requestMyLocation() {
-        Log.i(TAG, "start request location");
-        mLocationClient = new AMapLocationClient(getApplicationContext());
-        AMapLocationClientOption option = new AMapLocationClientOption();
-        option.setOnceLocation(true).setLocationMode(AMapLocationClientOption.AMapLocationMode.Device_Sensors).setMockEnable(true);
-        mLocationClient.setLocationOption(option);
-        mLocationClient.setLocationListener(mLocationListener);
-        mLocationClient.startLocation();
-    }
-
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-        updateMarker(latLng);
-    }
-
-    private void removeMarkers() {
-        if (null != mLastMarker) {
-            mLastMarker.remove();
-        }
-    }
-
-    public void getMyLoc(View view) {
-        requestMyLocation();
-    }
-
-    @Override
-    public void onCameraChange(CameraPosition cameraPosition) {
-
-    }
-
-    @Override
-    public void onCameraChangeFinish(CameraPosition cameraPosition) {
-        mZoomLevel = cameraPosition.zoom;
-        notifyPlayerPositionChanged();
-        Log.d(TAG,mAMap.getProjection().getVisibleRegion().latLngBounds.toString());
-    }
-
-    @Override
-    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
-        List<PoiItem> poiItems = regeocodeResult.getRegeocodeAddress().getPois();
-        StringBuilder loc = new StringBuilder();
-        loc.append(regeocodeResult.getRegeocodeAddress().getDistrict()).append(" ");
-        if (!poiItems.isEmpty()) {
-            loc.append(poiItems.get(0).getTitle());
-        } else {
-            loc.append(regeocodeResult.getRegeocodeAddress().getFormatAddress());
-        }
-        Log.d(TAG,loc.toString());
-        mLoc.setText(loc);
-    }
-
-    private void playWav(RadarImageEntity image) {
-        if (mLastGroundOverlay != null) {
-            mLastGroundOverlay.remove();
-        }
-        mLastGroundOverlay = mAMap.addGroundOverlay(new GroundOverlayOptions()
-                .anchor(0.5f, 0.5f).transparency(0.1f)
-                .image(BitmapDescriptorFactory.fromPath(image.getCachePath()))
-                .positionFromBounds(image.getArea()));
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis((long) (image.getTime() * 1000));
-        mTimeLine.setText(calendar.get(Calendar.HOUR_OF_DAY) + " : " + calendar.get(Calendar.MINUTE));
-    }
-
-    @Override
-    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
-
-    }
-
-    public void user(View view) {
-        Toast.makeText(this, "用户反馈", Toast.LENGTH_SHORT).show();
-        mPlayer.stop();
-    }
-
-    public void radio(View view) {
-        Toast.makeText(this, "雷达", Toast.LENGTH_SHORT).show();
-        mPlayer.start();
-    }
-
-    public void pause(View view) {
-        if (null == mPlayer) {
-            return;
-        }
-        if (mPlayer.isPlay()) {
-            mPlayer.pause();
-            mControlBtn.setText("start");
-        } else {
-            mPlayer.resume();
-            mControlBtn.setText("pause");
-        }
-    }
-
-    static class PlayHandler extends NoLeakHandler<MainActivity> {
-        public PlayHandler(MainActivity outClass) {
-            super(outClass);
-        }
-
-        @Override
-        public void handleMessage(Message msg, MainActivity mainActivity) {
-            if (null != mainActivity) {
-                switch (msg.what) {
-                    case RadarPlayer.MSG_PLAY:
-                        RadarImageEntity image = (RadarImageEntity) msg.obj;
-                        mainActivity.playWav(image);
-                        break;
-                    case RadarPlayer.MSG_STOP:
-                        mainActivity.clearRadioImg();
-                        break;
-                }
-            }
-        }
-    }
-
-    private void clearRadioImg() {
-        if (mLastGroundOverlay != null) {
-            mLastGroundOverlay.setVisible(false);
-            mLastGroundOverlay.remove();
-        }
-    }
+//
+//    private void updateMarker(LatLng latLng) {
+//        removeMarkers();
+//        LatLonPoint point = new LatLonPoint(latLng.latitude, latLng.longitude);
+//        RegeocodeQuery query = new RegeocodeQuery(point, 0, GeocodeSearch.AMAP);
+//        mGeocodeSearch.getFromLocationAsyn(query);
+//        mLastMarker = mAMap.addMarker(new MarkerOptions().position(latLng).icon(
+//                BitmapDescriptorFactory
+//                        .defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+//        notifyPlayerPositionChanged();
+//    }
+//
+//
+//    private void initView() {
+//
+//        mMapView = (MapView) findViewById(R.id.map_view);
+//        mLoc = (TextView) findViewById(R.id.loc_info);
+//        mControlBtn = (Button) findViewById(R.id.play_control);
+//        mGeocodeSearch = new GeocodeSearch(this);
+//        mGeocodeSearch.setOnGeocodeSearchListener(this);
+//        mImageView = (ImageView) findViewById(R.id.img);
+//        mTimeLine = (TextView) findViewById(R.id.time_line);
+//        mGdMapView = (GdMapView) findViewById(R.id.map);
+//        initMap();
+//    }
+//
+//    private void initMap() {
+//        mAMap = mMapView.getMap();
+//        mUiSettings = mAMap.getUiSettings();
+//        mUiSettings.setZoomControlsEnabled(false);
+//        mAMap.setOnMapClickListener(this);
+//        mAMap.setOnCameraChangeListener(this);
+//    }
+//
+//    private void requestMyLocation() {
+//        Log.i(TAG, "start request location");
+//        mLocationClient = new AMapLocationClient(getApplicationContext());
+//        AMapLocationClientOption option = new AMapLocationClientOption();
+//        option.setOnceLocation(true).setLocationMode(AMapLocationClientOption.AMapLocationMode.Device_Sensors).setMockEnable(true);
+//        mLocationClient.setLocationOption(option);
+//        mLocationClient.setLocationListener(mLocationListener);
+//        mLocationClient.startLocation();
+//    }
+//
+//
+//    @Override
+//    public void onMapClick(LatLng latLng) {
+//        updateMarker(latLng);
+//    }
+//
+//    private void removeMarkers() {
+//        if (null != mLastMarker) {
+//            mLastMarker.remove();
+//        }
+//    }
+//
+//    public void getMyLoc(View view) {
+//        requestMyLocation();
+//    }
+//
+//    @Override
+//    public void onCameraChange(CameraPosition cameraPosition) {
+//
+//    }
+//
+//    @Override
+//    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+//        mZoomLevel = cameraPosition.zoom;
+//        notifyPlayerPositionChanged();
+//        Log.d(TAG,mAMap.getProjection().getVisibleRegion().latLngBounds.toString());
+//    }
+//
+//    @Override
+//    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+//        List<PoiItem> poiItems = regeocodeResult.getRegeocodeAddress().getPois();
+//        StringBuilder loc = new StringBuilder();
+//        loc.append(regeocodeResult.getRegeocodeAddress().getDistrict()).append(" ");
+//        if (!poiItems.isEmpty()) {
+//            loc.append(poiItems.get(0).getTitle());
+//        } else {
+//            loc.append(regeocodeResult.getRegeocodeAddress().getFormatAddress());
+//        }
+//        Log.d(TAG,loc.toString());
+//        mLoc.setText(loc);
+//    }
+//
+//    private void playWav(RadarImageEntity image) {
+//        if (mLastGroundOverlay != null) {
+//            mLastGroundOverlay.remove();
+//        }
+//        mLastGroundOverlay = mAMap.addGroundOverlay(new GroundOverlayOptions()
+//                .anchor(0.5f, 0.5f).transparency(0.1f)
+//                .image(BitmapDescriptorFactory.fromPath(image.getCachePath()))
+//                .positionFromBounds(image.getArea()));
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis((long) (image.getTime() * 1000));
+//        mTimeLine.setText(calendar.get(Calendar.HOUR_OF_DAY) + " : " + calendar.get(Calendar.MINUTE));
+//    }
+//
+//    @Override
+//    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+//
+//    }
+//
+//    public void user(View view) {
+//        Toast.makeText(this, "用户反馈", Toast.LENGTH_SHORT).show();
+//        mPlayer.stop();
+//    }
+//
+//    public void radio(View view) {
+//        Toast.makeText(this, "雷达", Toast.LENGTH_SHORT).show();
+//        mPlayer.start();
+//    }
+//
+//    public void pause(View view) {
+//        if (null == mPlayer) {
+//            return;
+//        }
+//        if (mPlayer.isPlay()) {
+//            mPlayer.pause();
+//            mControlBtn.setText("start");
+//        } else {
+//            mPlayer.resume();
+//            mControlBtn.setText("pause");
+//        }
+//    }
+//
+//    static class PlayHandler extends NoLeakHandler<MainActivity> {
+//        public PlayHandler(MainActivity outClass) {
+//            super(outClass);
+//        }
+//
+//        @Override
+//        public void handleMessage(Message msg, MainActivity mainActivity) {
+//            if (null != mainActivity) {
+//                switch (msg.what) {
+//                    case RadarPlayer.MSG_PLAY:
+//                        RadarImageEntity image = (RadarImageEntity) msg.obj;
+//                        mainActivity.playWav(image);
+//                        break;
+//                    case RadarPlayer.MSG_STOP:
+//                        mainActivity.clearRadioImg();
+//                        break;
+//                }
+//            }
+//        }
+//    }
+//
+//    private void clearRadioImg() {
+//        if (mLastGroundOverlay != null) {
+//            mLastGroundOverlay.setVisible(false);
+//            mLastGroundOverlay.remove();
+//        }
+//    }
 
 }
